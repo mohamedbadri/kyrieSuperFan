1ff5dcb4c08c5e0c2d004c2df4c7900dcef6e7da
mohamedbadri <56170819+mohamedbadri@users.noreply.github.com>
2019-12-06 14:15:40 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,51 +4,142 @@ var c = document.getElementById("k3");
 var d = document.getElementById("k4");
 var e = document.getElementById("k5");
 var f = document.getElementById("k6");
+var g = document.getElementById("main1");
+var h = document.getElementById("main2");
+var i = document.getElementById("main3");
+var j = document.getElementById("bigmain2");
+var k = document.getElementById("title")
+var l = document.getElementById("ud")
+var m = document.getElementById("lg")
 
-function firstpair(){
-	document.getElementById("k2").classList.toggle("s1b");
-	document.getElementById("k3").classList.toggle("s1c");
-	document.getElementById("k4").classList.toggle("s1d");
-	document.getElementById("k5").classList.toggle("s1e");
-	document.getElementById("k6").classList.toggle("s1f");
+function firstPair(){
+	a.classList.toggle("s1a");
+	b.classList.toggle("s1b");
+	c.classList.toggle("s1c");
+	d.classList.toggle("s1d");
+	e.classList.toggle("s1e");
+	f.classList.toggle("s1f");
+	a.classList.toggle("black-white");
+	b.classList.toggle("black-white");
+	c.classList.toggle("black-white");
+	d.classList.toggle("black-white");
+	e.classList.toggle("black-white");
+	f.classList.toggle("black-white");
+	a.classList.toggle("cursor2");
 }
 
-function secondpair(){
-	document.getElementById("k1").classList.toggle("s2a");
-	document.getElementById("k3").classList.toggle("s2c");
-	document.getElementById("k4").classList.toggle("s2d");
-	document.getElementById("k5").classList.toggle("s2e");
-	document.getElementById("k6").classList.toggle("s2f");
+function secondPair(){
+	a.classList.toggle("s2a");
+	b.classList.toggle("s2b");
+	c.classList.toggle("s2c");
+	d.classList.toggle("s2d");
+	e.classList.toggle("s2e");
+	f.classList.toggle("s2f");
+	a.classList.toggle("black-white");
+	b.classList.toggle("black-white");
+	c.classList.toggle("black-white");
+	d.classList.toggle("black-white");
+	e.classList.toggle("black-white");
+	f.classList.toggle("black-white");
+	b.classList.toggle("cursor2");
 }
 
-function thirdpair(){
-	document.getElementById("k1").classList.toggle("s3a");
-	document.getElementById("k2").classList.toggle("s3b");
-	document.getElementById("k4").classList.toggle("s3d");
-	document.getElementById("k5").classList.toggle("s3e");
-	document.getElementById("k6").classList.toggle("s3f");
+function thirdPair(){
+	a.classList.toggle("s3a");
+	b.classList.toggle("s3b");
+	c.classList.toggle("s3c");
+	d.classList.toggle("s3d");
+	e.classList.toggle("s3e");
+	f.classList.toggle("s3f");
+	a.classList.toggle("black-white");
+	b.classList.toggle("black-white");
+	c.classList.toggle("black-white");
+	d.classList.toggle("black-white");
+	e.classList.toggle("black-white");
+	f.classList.toggle("black-white");
+	c.classList.toggle("cursor2");
 }
 
-function fourthpair(){
-	document.getElementById("k1").classList.toggle("s4a");
-	document.getElementById("k2").classList.toggle("s4b");
-	document.getElementById("k3").classList.toggle("s4c");
-	document.getElementById("k5").classList.toggle("s4e");
-	document.getElementById("k6").classList.toggle("s4f");
+function fourthPair(){
+	a.classList.toggle("s4a");
+	b.classList.toggle("s4b");
+	c.classList.toggle("s4c");
+	d.classList.toggle("s4d");
+	e.classList.toggle("s4e");
+	f.classList.toggle("s4f");
+	a.classList.toggle("black-white");
+	b.classList.toggle("black-white");
+	c.classList.toggle("black-white");
+	d.classList.toggle("black-white");
+	e.classList.toggle("black-white");
+	f.classList.toggle("black-white");
+	d.classList.toggle("cursor2");
 }
 
-function fithpair(){
-	document.getElementById("k1").classList.toggle("s5a");
-	document.getElementById("k2").classList.toggle("s5b");
-	document.getElementById("k3").classList.toggle("s5c");
-	document.getElementById("k4").classList.toggle("s5d");
-	document.getElementById("k6").classList.toggle("s5f");
+function fithPair(){
+	a.classList.toggle("s5a");
+	b.classList.toggle("s5b");
+	c.classList.toggle("s5c");
+	d.classList.toggle("s5d");
+	e.classList.toggle("s5e");
+	f.classList.toggle("s5f");
+	a.classList.toggle("black-white");
+	b.classList.toggle("black-white");
+	c.classList.toggle("black-white");
+	d.classList.toggle("black-white");
+	e.classList.toggle("black-white");
+	f.classList.toggle("black-white");
+	e.classList.toggle("cursor2");
 }
 
-function uncledrew(){
-	document.getElementById("main1").classList.toggle("uncledrew1");
-	document.getElementById("main2").classList.toggle("uncledrew2");
-	document.getElementById("main3").classList.toggle("uncledrew3");
-	document.getElementById("title").classList.toggle("title2");
+function sixPair(){
+	a.classList.toggle("s6a");
+	b.classList.toggle("s6b");
+	c.classList.toggle("s6c");
+	d.classList.toggle("s6d");
+	e.classList.toggle("s6e");
+	f.classList.toggle("s6f");
+	a.classList.toggle("black-white");
+	b.classList.toggle("black-white");
+	c.classList.toggle("black-white");
+	d.classList.toggle("black-white");
+	e.classList.toggle("black-white");
+	f.classList.toggle("black-white");
+	f.classList.toggle("cursor2");
+}
 
+function uncleDrew(){
+	g.classList.add("uncledrew1");
+	h.classList.add("uncledrew2");
+	i.classList.add("uncledrew3");
+	g.classList.remove("gif1");
+	h.classList.remove("gif2");
+	i.classList.remove("gif3");
+	j.classList.remove("green");
+	j.classList.add("orange");
+	k.classList.add("title2");
+	l.classList.remove("shake");
+	m.classList.add("shake");
+}
+
+function kyrieLogo(){
+	g.classList.remove("uncledrew1");
+	h.classList.remove("uncledrew2");
+	i.classList.remove("uncledrew3");
+	g.classList.remove("gif1");
+	h.classList.remove("gif2");
+	i.classList.remove("gif3");
+	j.classList.add("green");
+	j.classList.remove("orange");
+	k.classList.remove("title2");
+	l.classList.add("shake");
+	m.classList.remove("shake");
+	
+}
+
+function kyrieTitle(){
+	g.classList.add("gif1"); 
+	h.classList.add("gif2");
+	i.classList.add("gif3");
+	k.classList.remove("button-effect ");
 }
